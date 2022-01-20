2779b30c1ed4722cb485e7eb917e52e8650b463a
jmoon0714 <hyun.0714@gmail.com>
2022-01-19 22:58:25 -0500

    Server code refactor

--- client.py
+++ client.py
@@ -1,7 +1,10 @@
 import socket
 import os
 from _thread import *
+from datetime import datetime
 import threading
+import argparse
+from tqdm import tqdm
 import multiprocessing
 from multiprocessing import Process, Lock
 from datetime import timedelta
@@ -9,31 +12,84 @@ from PiSocketThread import PiSocketThread
 from PiSocketThread import PiSocketMultiThread
 from util import *
 
-if __name__ == '__main__':
-    multiprocessing.set_start_method('spawn')
-    barrier = threading.Barrier(BARRIER_THRESHOLD, action = None, timeout = None)
+class CaptureSession:
+    def __init__(self, dst):
+        self.dst_dir = dst
+        multiprocessing.set_start_method('spawn')
+        self.barrier = threading.Barrier(BARRIER_THRESHOLD, action = None, timeout = None)
+        self.sockets = []
 
-    n_threads = 0
-    sockets = []
-    for host_id in HOST_IDS:
-        host = '.'.join([NETWORK_ID, host_id])
-        print(host)
-        try:
-            # Create a TCP stream socket
-            client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-            client_socket.connect((host, PORT))
-        except socket.error as e:
-            print(str(e))
-        print(f'Connected to {host}')
-        client_socket.sendto(HANDSHAKE, (host, PORT))
+    def __handshake(self, client_socket, host, port):
+        client_socket.sendto(HANDSHAKE, (host, port))
         expected_host_id = client_socket.recvfrom(BUFFER_SIZE)[0].decode('utf-8')[-3:]
         if (expected_host_id not in HOST_IDS):
-            print(f'Unknown server {expected_host_id}. Closing socket')
-            client_socket.close()
-        print('Handshake successful')
-        PiSocketThread(client_socket, host, PORT, barrier).start() 
-        # p = Process(target=PiSocketMultiThread, args=(client_socket, host, PORT, barrier,))
-        # p.start()
-        # p.join()
-        n_threads+=1
-        print('Thread Count: ' + str(n_threads))
+            print(f'Unknown server {expected_host_id}.')
+            print('Handshake unsuccessful.')
+            return False
+        print('Handshake successful.')
+        return True
+
+    def connect(self):
+        for host_id in HOST_IDS:
+            if (host_id in BLOCK_LIST):
+                continue
+            host = '.'.join([NETWORK_ID, host_id])
+            # Create a TCP stream socket
+            try:
+                client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+                client_socket.connect((host, PORT))
+            except socket.error as e:
+                print(f'Could not connect to {host} because error: \n\t {str(e)}')
+                continue
+            print(f'Connected to {host}')
+            # Initiate Handshake
+            if (not self.__handshake(client_socket, host, PORT)):
+                client_socket.close()
+            # Successful Connection
+            self.sockets.append((client_socket, host, PORT))
+        print(f'{len(self.sockets)} sockets connected succesfully.')
+        return len(self.sockets)
+
+    def capture(self):
+        round=0
+        is_ready = input("Press enter to capture ('q' to quit). \n\t>")
+        while(is_ready != 'q'):
+            round+=1
+            print(f'Round {round}.')
+            threads = []
+            for client_socket, host, port in self.sockets:
+                pst = PiSocketThread(client_socket, host, port, self.barrier, round, self.dst_dir)
+                pst.start()
+                threads.append(pst) 
+                # p = Process(target=PiSocketMultiThread, args=(client_socket, host, PORT, barrier,))
+                # p.start()
+                # p.join()
+            for thread in tqdm(list(threads)):
+                thread.join()
+            is_ready = input("Press enter to capture ('q' to quit).\n\t>")
+    def close(self):
+        for s, address, port in self.sockets:
+            s.sendto(HANDWAVE, (address, port))
+            s.close()
+        
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Destination for image captures')
+    # date = (datetime.now()).strftime('%m.%d.%y %H:%M:%S')
+    date = (datetime.now()).strftime('%m.%d.%y')
+
+    parser.add_argument('--dst', dest='dst', default='/home/jmoon/Documents/CAIR/',
+                    help='Absolute path to destination dir')
+    parser.add_argument('--exp_no', dest='exp_no', default='X', help='Experiment number')
+    parser.add_argument('--session_name', dest='session_name', default=f'{date} Experiment {parser.parse_args().exp_no}',
+                    help='Session name')
+    args = parser.parse_args()
+    cwd = os.path.join(args.dst, args.session_name, 'data')
+    if (not os.path.exists(cwd)):
+        print(f'Created new dir: {cwd}')
+        os.makedirs(cwd)
+    session = CaptureSession(cwd)
+    if(session.connect()):
+        session.capture()
+        print('closing')
+        session.close()
---
+++ server.py
@@ -0,0 +1,86 @@
+import sched
+import socket
+from datetime import datetime
+from picamera import PiCamera
+from time import sleep
+import logging
+import traceback
+
+BUFFER_SIZE = 1024
+CAPTURE_FILEPATH = '/home/pi/Desktop/picture.png'
+HANDSHAKE = str.encode("Hello Pi Server")
+HANDWAVE = str.encode('Goodbye.')
+ip = '192.168.0.2' + socket.gethostname()[-2:]
+HOST, PORT = (ip, 20001)
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    filemode='w',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+def main():
+    # Setup TCP Socket
+    server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    try:
+        server_socket.bind((HOST, PORT))
+    except socket.error as e:
+        logging.error(e)
+        logging.info('Could not bind.')
+        return
+
+    # Setup Camera
+    camera = PiCamera()
+    # camera.framerate = 3
+    # camera.resolution = (3280, 2464)
+    camera.resolution = (1920, 1080)
+
+    logging.info('Waiting for a Connection...')
+    server_socket.listen(5)
+    while (True):
+        try:
+            client, _ = server_socket.accept()
+            expectedHandshake = client.recvfrom(BUFFER_SIZE)[0]
+            if(expectedHandshake != HANDSHAKE):
+                logging.error('Unknown Handshake.')
+                break
+            client.send(str.encode(HOST))
+            round = 1
+            logging.info('Connected!')
+            while(True):
+                expectedTimeOrHandwave = client.recvfrom(BUFFER_SIZE)
+                # Exit Condition
+                if(expectedTimeOrHandwave[0] == HANDWAVE or expectedTimeOrHandwave[0].decode('utf-8') == ''):
+                    server_socket.close()
+                    camera.close()
+                    return 1
+                if(expectedTimeOrHandwave[0] == ''):
+                    logging.info("huh?")
+                    continue
+
+                logging.info(f'Round {round} started.')
+                scheduledTime = datetime.strptime((expectedTimeOrHandwave[0].decode('utf-8')),'%y-%m-%d %H:%M:%S.%f')
+
+                logging.info(f"Sleeping for {(scheduledTime-datetime.now()).total_seconds()} seconds.")
+                assert (scheduledTime > datetime.now())
+                sleep((scheduledTime-datetime.now()).total_seconds())
+                camera.capture(CAPTURE_FILEPATH)
+                logging.info(f"Captured image at {datetime.now().strftime('%H:%M:%S')}")
+
+                # Send image to client
+                with open(CAPTURE_FILEPATH, 'rb') as file:
+                    image_data = file.read()
+                    image_data += 'DONE'.encode()
+                    client.sendall(image_data)
+                logging.info(f'Round {round} finished')
+                round+=1
+        except ValueError as e:
+            logging.error(traceback.print_exc())
+            logging.info('Waiting for a Connection...')
+        except AssertionError as e:
+            logging.error("No image taken because the scheduled time has already passed. GO FIND THE BUG!")
+            logging.error(traceback.print_exc())
+            logging.info('Waiting for a Connection...')
+    
+
+if __name__ == '__main__':
+    main()
